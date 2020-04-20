42edef3dc9027edd7cc8aec2a758440fa7b16c2d
RA1NB0W6666 <53051913+RA1NB0W6666@users.noreply.github.com>
2020-04-20 14:28:22 +0300

    ddddd
    ddddddddd

---
+++ config.js
@@ -0,0 +1,45 @@
+module.exports = {
+    token:          "NjkxNjkyNDY0MTgwMDM1NjQ1.XoRNIg.5BKtWMWhJdrgEHNH03D6equhVXo",
+    shardCount:     2,
+    /* Emojis */
+    emojis: {
+        success:    "8",
+        error:      "7",
+        online:     "6",
+        dnd:        "5",
+        offline:    "4",
+        idle:       "3",
+        loading:    "2",
+        warn:       "1"
+    },
+    /* Embeds */
+    color:          "#7289da",
+    footer:         "DavetYönetici",
+    /* Logs */
+    addLogs:        "693834463125438465",
+    removeLogs:     "693834463125438465",
+    shardLogs:      "693834463125438465",
+    dashLogs:       "693834463125438465",
+    statsLogs:      "693834463125438465",
+    /* Dashboard */
+    secret:         "XXXXXXXXXXX",
+    baseURL:        "XXXXXXXXXXX",
+    port:           3100,
+    pswd:           "XXXXXXXXXXX",
+    failureURL:     "https://docs.manage-invite.xyz",
+    /* Other */
+    mongodb:        "5e834c5c1785befd983cee0e",
+    discord:        "323171463832141826",
+    prefix:         "+",
+    owners: [ "323171463832141826", "598781976459345920" ],
+    /* Top.gg */
+    topToken: "XXXXXXXXXXX",
+    /* Database */
+    database: {
+        user: "books",
+        host: "127.0.0.1",
+        database: "davetyonetici",
+        password: "",
+        port: 27017
+    }
+};
